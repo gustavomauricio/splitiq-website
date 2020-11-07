7daffa79ef55d1edf9c36f78bf1b8aa4c99e008c
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2020-11-07 14:01:52 +0000

    home page responsiveness improved

--- pages/index.js
+++ pages/index.js
@@ -34,18 +34,18 @@ export default function Home() {
           Welcome to Split IQ
         </Title>
 
-        <Row gutter={16}>
-          <Col span={8}>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} lg={8}>
             <Card className="h-100" title="Card Random Title" bordered={false} hoverable>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat augue semper mollis ornare. Quisque bibendum est purus, non blandit felis laoreet eu. Proin rutrum magna rutrum, aliquet velit nec, laoreet nunc. Fusce efficitur lorem eget diam finibus rhoncus. Cras pellentesque quis lacus lobortis dapibus. Nunc elementum elementum turpis, ac malesuada elit semper eu. Donec sed auctor orci, id placerat leo. Integer tortor quam, cursus nec enim suscipit, consectetur molestie risus. Pellentesque sit amet ante nulla.
             </Card>
           </Col>
-          <Col span={8}>
+          <Col xs={24} lg={8}>
             <Card className="h-100" title="Card Random Title 2" bordered={false} hoverable>
               Donec tempor nec velit nec efficitur. Maecenas odio nulla, egestas in dignissim non, aliquet eget arcu. Cras consequat nec risus a rhoncus. Aliquam porta libero eu magna tempor dignissim. Morbi ac sem nec velit dictum accumsan. Etiam sagittis quam tempor sollicitudin dapibus. Cras non ornare sapien, sed congue lacus. Donec non fermentum justo. Nunc velit augue, eleifend ac lectus in, faucibus bibendum lorem. Curabitur in placerat ligula. Sed eu efficitur est. Praesent eu condimentum risus. Etiam id efficitur dolor.
             </Card>
           </Col>
-          <Col span={8}>
+          <Col xs={24} lg={8}>
             <Card className="h-100" title="Not so random title" bordered={false} hoverable>
               We are recruiting! Check <Link href="/recruitment">this</Link> section for more info.
             </Card>
@@ -53,8 +53,7 @@ export default function Home() {
         </Row>
         <Divider />
         <Row>
-          <Col span="16"></Col>
-          <Col span="8">
+          <Col xs={24} lg={{ span:8, offset: 16 }}>
             <Calendar fullscreen={false} />
           </Col>
         </Row>
